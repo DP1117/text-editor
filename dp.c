7f6f79ba4d834c4dc88b805aa44bc82cc928871a
Daniel Park <dpark070111@gmail.com>
2026-02-09 17:14:50 -0500

    enableRawMode complete

--- dp.c
+++ dp.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
@@ -6,36 +7,50 @@
 
 struct termios orig_termios;
 
+void die (const char *s) {
+    perror(s);
+    exit(1);
+}
 void disableRawMode (void) {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
 }
 
 void enableRawMode (void) {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
-   
-    raw.c_iflag &= ~(IXON | ICRNL);
+    
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_cflag |= (CS8);
+    raw.c_oflag &= ~(OPOST);
     raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+    raw.c_cc[VMIN] = 0;
+    // in milliseconds
+    raw.c_cc[VTIME] = 1;
 
     /* The TCSAFLUSH option specifies to set the terminal upon no 
      * more pending output while also discarding any unread input
      */
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
 int main (void) {
     // disables canonical and turns on raw
     enableRawMode();
 
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+    while (1) {
+        char c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+
         if (iscntrl(c)) {
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         } else {
-            printf("%d ('%c')\n", c, c);
+            printf("%d ('%c')\r\n", c, c);
         }
+
+        if (c == 'q') break;
     }
     return 0;
 }
